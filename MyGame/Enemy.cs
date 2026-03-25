8c2913ba16ece5f509628ee533676ce70ac5d12f
Daniel <daniel.henriques.gamedev@gmail.com>
2026-03-25 21:24:04 +0000

    finish exercice 7 and 8

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace MyGame
@@ -10,5 +11,29 @@ namespace MyGame
         private string name;
         private float health;
         private float shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
     }
 }
