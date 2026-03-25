52963f851442555d3511c7f87a3c5857d5906cd7
Daniel <daniel.henriques.gamedev@gmail.com>
2026-03-25 21:13:51 +0000

    finish exercice 5

---
+++ Chronos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch chrono1 = new Stopwatch();
+            Stopwatch chrono2 = new Stopwatch();
+
+            chrono1.Start();
+            Thread.Sleep(500);
+            chrono2.Start();
+            Thread.Sleep(250);
+
+            chrono1.Stop();
+            chrono2.Stop();
+
+            Console.WriteLine($"{chrono1.Elapsed.TotalSeconds:f4}");
+            Console.WriteLine($"{chrono2.Elapsed.TotalSeconds:f4}");
+        }
+    }
+}
---
+++ RandDice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(args[0]);
+            int s = Convert.ToInt32(args[1]);
+
+            Random rando = new Random(s);
+
+           int sum = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                
+                int temp = rando.Next(1,7);
+
+                sum += temp;
+                Console.WriteLine($"current calc{sum}");
+
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
